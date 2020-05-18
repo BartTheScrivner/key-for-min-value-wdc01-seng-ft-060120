f5350842bc561984de6803fbb3f25902e21cf49c
Dave Frame <dframe22@gmail.com>
2020-05-18 19:24:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   else
     name_hash.each do |(thing, amount)|
       minimum = nil
+      min_thing = ""
       if minimum == nil
         minimum = amount
         min_thing = thing
